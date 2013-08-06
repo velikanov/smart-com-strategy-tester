af51d85a86d600ae62f0cbd6c751fd0fca191534
Serge Velikanov <ceo@realtimein.com>
2013-08-06 09:43:51 +0400

    hello

---
+++ Objects/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    public class Server
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string Description { get; set; }
+        public bool Checked { get; set; }
+        public bool Default { get; set; }
+    }
+    public class ServerCollection
+    {
+        public ObservableCollection<Server> List;
+
+        public ServerCollection()
+        {
+            List = new ObservableCollection<Server>();
+        }
+    }
+}
